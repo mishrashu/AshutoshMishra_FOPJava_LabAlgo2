c0b89605944e58f50c2c98d02526b4bd0dc8b668
mishrashu <39207697+mishrashu@users.noreply.github.com>
2023-10-04 00:21:48 +0530

    Add files via upload

--- LabSession2Algorithms/src/com/ashutosh/currencycount/service/CurrencyCount.java
+++ LabSession2Algorithms/src/com/ashutosh/currencycount/service/CurrencyCount.java
@@ -9,22 +9,22 @@ public class CurrencyCount {
 		try {
 			for (int i = 0; i < currencyDenominations.length; i++) {
 
-				if (amountToPay > currencyDenominations[i]) {
+				if (amountToPay >= currencyDenominations[i]) {
 					currencyCounter[i] = amountToPay / currencyDenominations[i];
 					amountToPay = amountToPay - (currencyCounter[i] * currencyDenominations[i]);
 
 					if (amountToPay == 0)
 						break;
 				}
+			}
 
-				if (amountToPay > 0) {
-					System.out.println("exact amount can not be given with the heightest denomination");
-				} else {
-					System.out.println("Your payment approach in order to give min no of notes will be");
-					for (int j = 0; i < currencyDenominations.length; j++) {
-						if (currencyCounter[j] != 0) {
-							System.out.println(currencyDenominations[j] + ":" + currencyCounter[i]);
-						}
+			if (amountToPay > 0) {
+				System.out.println("exact amount can not be given with the heightest denomination");
+			} else {
+				System.out.println("Your payment approach in order to give min no of notes will be");
+				for (int i = 0; i < currencyDenominations.length; i++) {
+					if (currencyCounter[i] != 0) {
+						System.out.println(currencyDenominations[i] + ":" + currencyCounter[i]);
 					}
 				}
 			}
